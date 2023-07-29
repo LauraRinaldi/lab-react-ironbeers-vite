56eeb882267215ea32d48d9f5ccfa35714fba9f4
Laura Rinaldi <rinaldilaurinha@gmail.com>
2023-07-29 09:28:42 -0400

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,44 @@
 import "./App.css";
+import { Routes, Route, Link} from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import HomePage from "./pages/HomePage";
+import AllBeersPage from "./pages/AllBeersPage";
+
 
 function App() {
+
+   const [beers, setBeers] = useState([])
+  
+   useEffect( () => {
+    axios.get('https://ih-beers-api2.herokuapp.com/beers')
+        .then((response) => {
+          console.log("Beers:", response.data)
+          setCountries(response.data)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+  }, [])
+
   return (
     <div className="App">
       <h1>LAB | React IronBeers</h1>
+      <Navbar/>
+      <Routes>
+
+      <Route path="/" element={<HomePage />}/>
+      <Route path="/beers" element={<AllBeersPage beers={beers} />} />
+      <Route path="/random-beer" element={<RandomBeerPage />}/>
+      <Route path="/new-beer" element={<AddBeersPage />} />
+      <Route path="/beers/:beersId" element={<BeerDetailsPage beers={beers}/>}/>
+      
+    </Routes>
     </div>
   );
 }
+ 
+
+
 
 export default App;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,16 @@
-function HomePage() {}
+import { Link } from "react-router-dom"
 
-export default HomePage;
+const HomePage = () => {
+  return (
+    <div className="home">
+
+        <Link to='/beers'><h1>All Beers</h1></Link>
+        <Link to='/random-beer'><h1>Random Beer</h1></Link>
+        <Link to='/new-beer'><h1>Add Beer</h1></Link>
+         
+
+    </div>
+  )
+}
+
+export default HomePage
